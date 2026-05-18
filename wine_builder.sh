c82698c5bf42308f120f415e4f69bfa040d75f72
NelloKudo <marshnelloosu@gmail.com>
2026-05-18 15:56:03 +0200

    bump: Spritz-Wine-CachyOS 10.0-11
    - Fixed an issue in crypt32 that prevented CN GI/ZZZ from working

--- wine_builder.sh
+++ wine_builder.sh
@@ -36,7 +36,7 @@ _configuration() {
     WINE_VERSION=''
     STAGING_VERSION=''
     WINE_BRANCH="${WINE_BRANCH:-}"
-    RELEASE_VERSION='10'
+    RELEASE_VERSION='11'
     PATCHSET=''
 
     # Build configuration
